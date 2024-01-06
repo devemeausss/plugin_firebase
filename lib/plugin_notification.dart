f45582bc70ee62aa07f456ba115bb6309121ec3d
dev au <tharo@devx.com.au>
2024-01-06 20:16:35 +0700

    Upgrade lib

--- lib/plugin_notification.dart
+++ lib/plugin_notification.dart
@@ -72,7 +72,7 @@ class MyPluginNotification {
         await _flutterLocalNotificationsPlugin
             .resolvePlatformSpecificImplementation<
                 AndroidFlutterLocalNotificationsPlugin>()!
-            .requestPermission();
+            .requestNotificationsPermission();
       }
       _flutterLocalNotificationsPlugin.initialize(initializationSettings,
           onDidReceiveNotificationResponse: (NotificationResponse? data) async {
